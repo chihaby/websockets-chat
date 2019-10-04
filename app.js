1c3155e9eb2b5549a868a91eaa7d5944f3e3d64c
Rad <chihaby@gmail.com>
2019-10-04 13:21:10 -0700

    sets up the backend

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
-const feathers = require ('@feathersjs/feathers');
-const express = require ('@feathersjs/express');
-const socketio = require ('@feathersjs/socketio');
-const moment = require ('moment');
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+const moment = require('moment');
 
-// Ideas Service
-class IdeaSevice {
-    constructor() {
-        this.ideas = [];
-    }
+// Idea Service
+class IdeaService {
+  constructor() {
+    this.ideas = [];
+  }
 
-    async find() {
-        return this.ideas;
-    }
+  async find() {
+    return this.ideas;
+  }
 
-    async create(data) {{
-        const idea = {
-            id: this.ideas.length,
-            text: data.text,
-            tech: data.tech,
-            viewer: data.viewer
-        }
+  async create(data) {
+    const idea = {
+      id: this.ideas.length,
+      text: data.text,
+      tech: data.tech,
+      viewer: data.viewer
+    };
 
-        idea.time = moment().format('h:mm:ss a');
+    idea.time = moment().format('h:mm:ss a');
 
-        this.ideas.push(idea);
-        return idea;
-    }
+    this.ideas.push(idea);
+
+    return idea;
+  }
 }
 
 const app = express(feathers());
 
-// Parse JASON
+// Parse JSON
 app.use(express.json());
-
 // Config Socket.io realtime APIs
 app.configure(socketio());
-
-// Enable REST sevices
+// Enable REST services
 app.configure(express.rest());
-
 // Register services
-app.use('/ideas', new IdeaSevice());
+app.use('/ideas', new IdeaService());
 
 // New connections connect to stream channel
 app.on('connection', conn => app.channel('stream').join(conn));
-
 // Publish events to stream
 app.publish(data => app.channel('stream'));
 
-PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 
-app.listen(PORT).on('listening', ()=> console.log(`Realtime serv${PORT}`) );
+app
+  .listen(PORT)
+  .on('listening', () =>
+    console.log(`Realtime server running on port ${PORT}`)
+  );
+
+app.service('ideas').create({
+  text: 'Build a cool app',
+  tech: 'Node.js',
+  viewer: 'John Doe'
+});
